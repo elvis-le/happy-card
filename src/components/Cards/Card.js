f133f212fb05e266c41d086fa863a87dbac98558
elvis-le <khanh321zx@gmail.com>
2025-03-28 18:36:42 +0700

    first commit

---
+++ src/components/Cards/Card.js
@@ -0,0 +1,114 @@
+import logo from "../assets/logo.png";
+import './Card.scss'
+import {MdCreate, MdOutlineFavorite, MdOutlineFavoriteBorder} from "react-icons/md";
+import Footer from "./Footer";
+import {GrNext, GrPrevious} from "react-icons/gr";
+import {useEffect, useRef, useState} from "react";
+import Navigation from "./Navigation";
+import {useNavigate, useParams} from "react-router-dom";
+
+const Card = () => {
+    const { cardId } = useParams();
+    const visibleImages = 4;
+    const images = ["https://picsum.photos/200/300?random=1", "https://picsum.photos/200/300?random=2", "https://picsum.photos/200/300?random=3", "https://picsum.photos/200/300?random=4", "https://picsum.photos/200/300?random=5", "https://picsum.photos/200/300?random=6", "https://picsum.photos/200/300?random=7", "https://picsum.photos/200/300?random=8", "https://picsum.photos/200/300?random=9"];
+
+    const extendedImages = [...images, ...images];
+    const totalImages = images.length;
+
+    const [index, setIndex] = useState(0);
+    const listRef = useRef(null);
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        if (index === totalImages) {
+            setTimeout(() => {
+                listRef.current.style.transition = "none";
+                setIndex(0);
+            }, 500); // Reset vị trí về đầu khi chạm ảnh cuối
+        }
+    }, [index, totalImages]);
+
+    const nextImage = () => {
+        if (index >= totalImages) return;
+        setIndex(index + 1);
+        listRef.current.style.transition = "transform 0.5s ease-in-out";
+    };
+
+    const prevImage = () => {
+        if (index <= 0) {
+            setTimeout(() => {
+                listRef.current.style.transition = "none";
+                setIndex(totalImages - 1);
+            }, 500);
+        } else {
+            setIndex(index - 1);
+            listRef.current.style.transition = "transform 0.5s ease-in-out";
+        }
+    };
+
+    const handleClick = () => {
+        navigate(`/EditCard/${cardId}`);
+    };
+
+    return (
+        <div className="card-wrapper">
+            <div className="card-wrap">
+                <Navigation/>
+                <div className="card-component">
+                    <div className="card-detail-wrapper">
+                        <div className="card-detail-wrap">
+                            <div className="card-video-wrap">
+                                <div className="card-video">
+                                    <video></video>
+                                </div>
+                                <div className="card-video-option">
+                                    <div className="previous-btn" onClick={prevImage}>
+                                        <GrPrevious/>
+                                    </div>
+                                    <div className="image-container">
+                                        <div
+                                            className="image-list"
+                                            ref={listRef}
+                                            style={{transform: `translateX(-${index * 24.95}%)`}}
+                                        >
+                                            {extendedImages.map((src, i) => (
+                                                <img key={i} src={src} alt={`preview-${i}`}/>))}
+                                        </div>
+                                    </div>
+                                    <div className="next-btn" onClick={nextImage}>
+                                        <GrNext/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-detail-video-wrap">
+                                <div className="card-detail">
+                                    <div className="card-detail-header-wrap">
+                                        <h3 className="card-detail-header">Birthday Card</h3>
+                                    </div>
+                                    <div className="card-detail-content">
+                                        <span>Welcome to this special birthday card! This is your chance to share heartfelt wishes, meaningful words, and all the best for someone you care about. Let your message bring joy, happiness, and make their birthday even more memorable.
+                                        <br/>Write from the heart and turn this card into a beautiful gift of love and kindness! 🎂🎉💖</span>
+                                    </div>
+                                    <div className="card-detail-btn">
+                                        <div className="card-favorite-btn">
+                                            <span>Favorite</span>
+                                            <MdOutlineFavoriteBorder/>
+                                            <MdOutlineFavorite/>
+                                        </div>
+                                        <div className="card-edit-btn" onClick={() => handleClick()}>
+                                            <span>Edit</span>
+                                            <MdCreate/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer/>
+        </div>)
+}
+
+export default Card;
---
+++ src/components/Cards/EditCard.js
@@ -0,0 +1,240 @@
+import Navigation from "./Navigation";
+import birthdayCard from '../assets/birthdaycard.png'
+import birthdayCardCover from '../assets/birthdaycard-cover.png'
+import './EditCard.scss'
+import Footer from "./Footer";
+import {useState} from "react";
+
+const EditCard = () => {
+    const [title, setTitle] = useState("");
+    const [sender, setSender] = useState("");
+    const [receiver, setReceiver] = useState("");
+    const [content, setContent] = useState("");
+
+    const [isTitleForm, setTitleForm] = useState(true);
+
+    const handleClickContent = (e) => {
+        setContent(e.target.textContent);
+    };
+
+    const handleClickTitle = (e) => {
+        setTitle(e.target.textContent);
+    };
+
+    return (
+        <div className="edit-card-wrapper">
+            <div className="edit-card-wrap">
+                <Navigation/>
+                <div className="edit-card-wrap-component">
+                    <div className="edit-card-form-wrapper">
+                        <div className="edit-card-form-wrap">
+                            <div className="edit-card-form-header">
+                                <h3>Birthday Card</h3>
+                            </div>
+                            {isTitleForm &&
+                                <div className="edit-form-title-wrapper">
+                                    <div className="edit-card-form-title-wrap">
+                                        <div className="edit-card-form-title">
+                                            Title:
+                                        </div>
+                                        <input type="text" name="title" className="title-name"
+                                               value={title} onChange={(e) => setTitle(e.target.value)}/>
+                                    </div>
+                                </div>
+                            }
+                            {!isTitleForm &&
+                                <div className="edit-form-content-wrapper">
+                                    <div className="edit-card-form-sender-wrap">
+                                        <div className="edit-card-form-sender">
+                                            Sender:
+                                        </div>
+                                        <input type="text" name="sender" className="sender-name"
+                                               value={sender} onChange={(e) => setSender(e.target.value)}/>
+                                    </div>
+                                    <div className="edit-card-form-receiver-wrap">
+                                        <div className="edit-card-form-receiver">
+                                            Receiver:
+                                        </div>
+                                        <input type="text" name="receiver" className="receiver-name"
+                                               value={receiver} onChange={(e) => setReceiver(e.target.value)}/>
+                                    </div>
+                                    <div className="edit-card-form-content-wrap">
+                                        <div className="edit-card-form-content">
+                                            Content:
+                                        </div>
+                                        <textarea name="content" className="content"
+                                                  value={content} onChange={(e) => setContent(e.target.value)}/>
+                                    </div>
+                                </div>
+                            }
+                            <div className="suggest-content-option-wrap">
+                                <div className="suggest-content-type-option">
+                                    <div className="content-type-option">Choose content</div>
+                                    <div className="content-type-option">Love</div>
+                                    <div className="content-type-option">Husband</div>
+                                    <div className="content-type-option">Wife</div>
+                                    <div className="content-type-option">Friend</div>
+                                </div>
+                                <div className="suggest-content-option">
+                                    {!isTitleForm &&
+                                        <>
+                                            <div className="content-option" onClick={handleClickContent}>🎉 Happy
+                                                Birthday! May
+                                                this
+                                                year bring you happiness,
+                                                success, and all your heart’s desires. 🎂🎁
+                                            </div>
+                                            <div className="content-option" onClick={handleClickContent}>🤣 Happy
+                                                Birthday!
+                                                Don’t
+                                                count
+                                                the candles, just
+                                                enjoy
+                                                the glow. Wishing you a fantastic year ahead! 🎂🔥
+                                            </div>
+                                            <div className="content-option" onClick={handleClickContent}>💖 Happy
+                                                Birthday, my
+                                                love!
+                                                Every moment with you is
+                                                special, and I’m so lucky to have you by my side. Love you forever! 😘🎈
+                                            </div>
+                                            <div className="content-option" onClick={handleClickContent}>🎊 Happy
+                                                Birthday,
+                                                bestie! May
+                                                your day be as amazing
+                                                as you are. Let’s make unforgettable memories together! 🥳🎁
+                                            </div>
+                                            <div className="content-option" onClick={handleClickContent}>🎉 Wishing you a
+                                                wonderful
+                                                birthday filled with joy
+                                                and success. May this year bring you prosperity and happiness! 🎂🎈
+                                            </div>
+                                            <div className="content-option" onClick={handleClickContent}>🍀 Happy
+                                                Birthday!
+                                                Wishing you
+                                                a year full of good
+                                                health, laughter, and endless happiness. Stay amazing! 💪🎉
+                                            </div>
+                                            <div className="content-option" onClick={handleClickContent}>🚀 Happy
+                                                Birthday! May
+                                                you
+                                                reach new heights in your
+                                                career and personal life. Keep shining! ✨🎂
+                                            </div>
+                                            <div className="content-option" onClick={handleClickContent}>❤️ Wishing you
+                                                a day
+                                                filled
+                                                with love, laughter, and
+                                                everything that brings you joy. Happy Birthday! 🎁🎈
+                                            </div>
+                                            <div className="content-option" onClick={handleClickContent}>🎶 Another year,
+                                                another
+                                                adventure! May your birthday
+                                                be the start of a fantastic journey ahead. 🥂🎂
+                                            </div>
+                                            <div className="content-option" onClick={handleClickContent}>🌟 You are one
+                                                of a
+                                                kind, and
+                                                today is the perfect
+                                                day to celebrate you! Wishing you a birthday as special as you are. 🎉💖
+                                            </div>
+                                        </>
+                                    }
+                                    {isTitleForm &&
+                                        <>
+                                            <div className="content-option" onClick={handleClickTitle}>Happy Birthday!
+                                            </div>
+                                            <div className="content-option" onClick={handleClickTitle}>🤣 Happy Birthday!
+                                            </div>
+                                            <div className="content-option" onClick={handleClickTitle}>💖 Happy Birthday!
+                                                😘🎈
+                                            </div>
+                                            <div className="content-option" onClick={handleClickTitle}>🎊 Happy
+                                                Birthday,! 🥳🎁
+                                            </div>
+                                            <div className="content-option" onClick={handleClickTitle}>🎉 Wishing you a
+                                                wonderful
+                                                birthday filled with joy
+                                                and success! 🎂🎈
+                                            </div>
+                                            <div className="content-option" onClick={handleClickTitle}>🍀 Happy Birthday!
+                                            </div>
+                                            <div className="content-option" onClick={handleClickTitle}>🚀 Happy Birthday!
+                                            </div>
+                                            <div className="content-option" onClick={handleClickTitle}>Happy Birthday!
+                                                🎁🎈
+                                            </div>
+                                            <div className="content-option" onClick={handleClickTitle}>🎶 Another year,
+                                                another
+                                                adventure!🥂🎂
+                                            </div>
+                                            <div className="content-option" onClick={handleClickTitle}>Wishing you a
+                                                birthday as special as you are. 🎉💖
+                                            </div>
+                                        </>
+                                    }
+
+                                </div>
+                            </div>
+                            <div className="create-card-btn-wrap">
+                                <div className="next-card-btn">
+                                    Next
+                                </div>
+                                <div className="previous-card-btn">
+                                    Previous
+                                </div>
+                                <div className="add-new-card-btn">
+                                    Add new page
+                                </div>
+                                <div className="view-card-btn">
+                                    View
+                                </div>
+                                <div className="create-card-btn">
+                                    Create
+                                </div>
+                            </div>
+                        </div>
+                        <div className="edit-card-view-wrap">
+                            <div className="card-view-edit-wrap">
+                                <div className="card-view-edit-image">
+                                    {isTitleForm &&
+                                        <img src={birthdayCardCover} alt="card-view"/>
+                                    }
+                                    {!isTitleForm &&
+                                        <img src={birthdayCard} alt="card-view"/>
+                                    }
+                                </div>
+                                <div className="view-content-edit">
+                                    {isTitleForm &&
+                                        <div className="preview-title">
+                                            <p>{title}</p>
+                                        </div>
+                                    }
+                                    {!isTitleForm &&
+                                        <>
+                                            <div className="preview-receiver">
+                                                Deer{" "}
+                                                <span>{sender}</span>
+                                                ,
+                                            </div>
+                                            <div className="preview-content">
+                                                <p>{content}</p>
+                                            </div>
+                                            <div className="preview-sender">
+                                                From{" "}
+                                                <span>{receiver}</span>
+                                            </div>
+                                        </>
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Footer/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EditCard;
